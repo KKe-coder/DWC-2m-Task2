2ad24a29faf490a55d63f8b27d3a38e1919ad1d7
KKe-coder <carumag726@gmail.com>
2020-12-01 13:05:11 +0000

    RSpectest実行:残りerror

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -50,4 +50,10 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
+  def ensure_correct_user
+      @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
 end
