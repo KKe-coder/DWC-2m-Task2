03cd204415efc0e577ecb895986f99b4d963fbc1
KKe-coder <carumag726@gmail.com>
2020-12-02 00:42:22 +0000

    [Finish] User Infoの細かな修正,READMe.md追加,RSpecテスト完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,6 +25,10 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user.id == current_user.id
+    else
+        redirect_to books_path
+    end
   end
 
 
